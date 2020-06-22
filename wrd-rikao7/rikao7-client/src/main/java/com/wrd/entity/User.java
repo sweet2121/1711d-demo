54b5144821bce1e38f1ad2c4bcdaee2764d4d4bd
sweet2121 <2415726082@qq.com>
2020-06-22 17:20:25 +0800

    未来可期

--- wrd-rikao7/rikao7-client/src/main/java/com/wrd/entity/User.java
+++ wrd-rikao7/rikao7-client/src/main/java/com/wrd/entity/User.java
@@ -26,11 +26,11 @@ public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @TableId(value = "id", type = IdType.AUTO)
-    private Integer id;
+    private Integer id;//id
 
-    private String username;
+    private String username;//用户名
 
-    private LocalDate time;
+    private LocalDate time;//创建时间
 
 
 }
